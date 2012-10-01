1298040bad503290280574f326bff8a8270dabbf
Álvaro Castro-Castilla <alvcastro@yahoo.es>
2012-10-01 21:06:40 +0200

    trying to get the suitable features/version/options way

--- sakefile.scm
+++ sakefile.scm
@@ -17,18 +17,18 @@
                (sake:compile-c-file (sake:generate-c-file m)
                                     cc-options: cc-options
                                     ld-options: ld-options)
-               (sake:compile-c-file (sake:generate-c-file m features: '(mobile debug))
+               (sake:compile-c-file (sake:generate-c-file m
+                                                          version: '(debug)
+                                                          compiler-options: '(debug))
                                     cc-options: cc-options
                                     ld-options: ld-options))
              modules)))
-
-;; TODO:       (define-cond-expand-feature arm)
 
 (define-task install (compile)
   (make-directory (default-lib-directory))
   (for-each (lambda (m)
               (sake:install-compiled-module m)
-              (sake:install-compiled-module m features: '(mobile debug)))
+              (sake:install-compiled-module m version: '(debug)))
             modules))
 
 (define-task all (install)
