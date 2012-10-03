ef9213d874e8873edb44fd17d461ec4cdf98cea1
Álvaro Castro-Castilla <alvcastro@yahoo.es>
2012-10-03 15:12:30 +0200

    updated sakefile to newest sphere system

--- config.scm
+++ config.scm
@@ -1,4 +1,1 @@
-(project-name: "cairo")
-(paths:
- (base: "/data/projects/scheme-base")
- (sake: "/data/projects/scheme-sake"))
+(sphere: "cairo")
--- sakefile.scm
+++ sakefile.scm
@@ -1,35 +1,34 @@
-(include "~~base/prelude#.scm")
+(include "~~spheres/prelude#.scm")
 (%include sake: utils#)
 
 (define modules '(cairo))
 
-(define-task init ()
-  (make-directory (current-build-directory)))
+(define-task clean ()
+  (sake:default-clean))
 
-(define-task clean (init)
-  (delete-file (current-build-directory))
-  (delete-file (default-lib-directory)))
-
-(define-task compile (init)
+(define-task compile ()
   (let ((cc-options "-w -I/usr/include/cairo -I/usr/include/freetype2")
         (ld-options "-lcairo -lfreetype"))
-   (for-each (lambda (m)
-               (sake:compile-c-file (sake:generate-c-file m)
-                                    cc-options: cc-options
-                                    ld-options: ld-options)
-               (sake:compile-c-file (sake:generate-c-file m
-                                                          version: '(debug)
-                                                          compiler-options: '(debug))
-                                    cc-options: cc-options
-                                    ld-options: ld-options))
-             modules)))
+    (for-each (lambda (m)
+                (sake:compile-c-file (sake:generate-c-file m)
+                                     cc-options: cc-options
+                                     ld-options: ld-options)
+                (sake:compile-c-file (sake:generate-c-file m
+                                                           version: '(debug)
+                                                           compiler-options: '(debug))
+                                     cc-options: cc-options
+                                     ld-options: ld-options))
+              modules)))
 
-(define-task install (compile)
-  (make-directory (default-lib-directory))
+(define-task install ()
   (for-each (lambda (m)
               (sake:install-compiled-module m)
               (sake:install-compiled-module m version: '(debug)))
-            modules))
+            modules)
+  (sake:install-system-sphere))
 
-(define-task all (install)
+(define-task uninstall ()
+  (sake:uninstall-system-sphere))
+
+(define-task all (compile install)
   'all)
