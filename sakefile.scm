03c2de7c477bd5e5059a426e13e1bb561d915096
Álvaro Castro-Castilla <alvcastro@yahoo.es>
2012-11-09 12:59:16 +0100

    includes are now handled by sake

--- sakefile.scm
+++ sakefile.scm
@@ -1,22 +1,17 @@
-(include "~~spheres/prelude#.scm")
-(%include sake: utils#)
-
 (define modules '(cairo))
 
 (define-task clean ()
   (sake:default-clean))
 
 (define-task compile ()
-  (let ((include '((base: ffi#)))
-        (cc-options "-w -I/usr/include/cairo -I/usr/include/freetype2")
+  (let ((cc-options "-w -I/usr/include/cairo -I/usr/include/freetype2")
         (ld-options "-lcairo -lfreetype"))
     (for-each (lambda (m)
-                (sake:compile-c-to-o (sake:compile-to-c m include: include)
+                (sake:compile-c-to-o (sake:compile-to-c m)
                                      cc-options: cc-options
                                      ld-options: ld-options)
                 (sake:compile-c-to-o (sake:compile-to-c m
                                                         version: '(debug)
-                                                        include: include
                                                         compiler-options: '(debug))
                                      cc-options: cc-options
                                      ld-options: ld-options))
