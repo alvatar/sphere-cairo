6b3a4f3473aea4b55779df5ef339c9e33041b952
Álvaro Castro-Castilla <alvcastro@yahoo.es>
2013-01-19 21:34:47 +0100

    renamed procedure

--- sakefile.scm
+++ sakefile.scm
@@ -14,10 +14,10 @@
 
 (define-task install ()
   (for-each sake:install-compiled-module modules)
-  (sake:install-system-sphere))
+  (sake:install-sphere-to-system))
 
 (define-task uninstall ()
-  (sake:uninstall-system-sphere))
+  (sake:uninstall-sphere-from-system))
 
 (define-task all (compile install)
   'all)
