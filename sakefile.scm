69e545c38fa3e6981e4bee519e5aed7510c13b01
Álvaro Castro-Castilla <alvcastro@yahoo.es>
2014-06-09 21:26:25 +0200

    make it compile in OSX when Cairo is installed via Homebrew

--- sakefile.scm
+++ sakefile.scm
@@ -3,8 +3,13 @@
 
 
 (define-task compile ()
-  (for-each (lambda (m) (sake#compile-module m compiler-options: '(debug))) modules)
-  (for-each sake#compile-module modules))
+  ;; This fixes and issue in OSX when Cairo is installed with Homebrew
+  (if (eq? (sake#host-platform) 'osx)
+      (setenv "PKG_CONFIG_PATH" "/opt/X11/lib/pkgconfig/"))
+  (for-each (lambda (m)
+              (sake#compile-module m cond-expand-features: '(debug) version: '(debug))
+              (sake#compile-module m cond-expand-features: '(optimize) verbose: #t))
+            modules))
 
 (define-task post-compile ()
   (for-each (lambda (m) (sake#make-module-available m versions: '(() (debug)))) modules))
