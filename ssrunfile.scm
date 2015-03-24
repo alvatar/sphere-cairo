3cba05571b4a3f66fcf7940d9e093778b42cb61d
Álvaro Castro-Castilla <alvcastro@yahoo.es>
2015-03-24 19:00:44 +0100

    Deprecated sakefile and created ssrunfile

---
+++ ssrunfile.scm
@@ -0,0 +1,48 @@
+(define libraries
+  `(((spheres/cairo cairo)
+     .
+     ,(lambda (lib)
+        (if (eq? 'osx (ssrun#host-platform))
+            (begin
+              ;; This fixes and issue in OSX when Cairo is installed with Homebrew
+              (setenv "PKG_CONFIG_PATH" "/opt/X11/lib/pkgconfig/")
+              (let ((cc-options
+                     (string-append
+                      (with-input-from-process
+                       (list path: "pkg-config"
+                             arguments: '("--cflags" "cairo")) read-line)
+                      "-w"))
+                    (ld-options
+                     (with-input-from-process
+                      (list path: "pkg-config"
+                            arguments: '("--libs" "cairo"))
+                      read-line)))
+                (ssrun#compile-library
+                 lib
+                 cond-expand-features: '(debug)
+                 cc-options: cc-options
+                 ld-options: ld-options)))
+            (ssrun#compile-library
+             lib
+             cond-expand-features: '(debug)
+             cc-options: "-w"
+             ld-options: ""))))))
+
+(define-task (compile library) ()
+  ;; This fixes and issue in OSX: pkg-config not finding GL
+  ;; (if (eq? (sake#host-platform) 'osx)
+  ;;     (setenv "PKG_CONFIG_PATH" "/opt/X11/lib/pkgconfig/"))
+  (if library
+      (let ((entry (assoc library libraries))) ((cdr entry) (car entry)))
+      (for-each (lambda (entry) ((cdr entry) (car entry))) libraries)))
+
+(define-task (test file) ()
+  (if file
+      (ssrun#run-file (string-append "test/" file))
+      (ssrun#run-all-files "test/")))
+
+(define-task clean ()
+  (ssrun#clean-libraries (map car libraries)))
+
+(define-task all (compile)
+  (void))
